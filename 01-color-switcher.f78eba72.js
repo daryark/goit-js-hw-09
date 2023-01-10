22cdaa2d6a26ae41fa4346882b050f54ea82794e
daryark <81414858+daryark@users.noreply.github.com>
2023-01-10 19:40:57 +0000

    Deploying to gh-pages from @ daryark/goit-js-hw-09@28b8de8b538975d688e1bce2be762c7263cbdb28 🚀

---
+++ 01-color-switcher.f78eba72.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start"),e=document.querySelector("[data-stop"),a=null;t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled&&(e.disabled=!1),a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(e){e.target.disabled=!0,t.disabled&&(t.disabled=!1),clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.f78eba72.js.map
