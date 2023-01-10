09869b1fa0baa5dbcde17a62c5c5f0e491c89274
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-10 20:25:45 +0200

    task 1 + Initial commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+const startBtn = document.querySelector('[data-start');
+const stopBtn = document.querySelector('[data-stop');
+let timerId = null;
+
+function changeBodyColor(e) {
+  e.target.disabled = true;
+
+  if (stopBtn.disabled) {
+    stopBtn.disabled = false;
+  }
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeBodyColor(e) {
+  e.target.disabled = true;
+
+  if (startBtn.disabled) {
+    startBtn.disabled = false;
+  }
+  clearInterval(timerId);
+}
+
+startBtn.addEventListener('click', changeBodyColor);
+stopBtn.addEventListener('click', stopChangeBodyColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
